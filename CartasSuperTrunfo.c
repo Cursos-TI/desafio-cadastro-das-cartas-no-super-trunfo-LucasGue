6070dd4aa06ce32d6d32cf2e45963ec48331fd23
LucasGue <87082917+LucasGue@users.noreply.github.com>
2025-01-21 00:08:29 +0000

    Cadastro de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,43 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("Desafio SuperTrunfo!\n");
+
+    float populacao, area, pib;
+    int pontosturisticos, estado;
+    char cidade[1];
+
+// Início do jogo, cadastrando a carta
+
+    printf(" --- Bem vindo ao SuperTrunfo de Países! ---\n\n");
+    printf("Para começar, cadastre sua carta! \n");
+    printf("Cidade: \n");
+    scanf("%s" &cidade);
+    printf("Agora, cadastre seu estado: \n");
+    scanf("%d" %estado);
+    printf("Qual a população da sua cidade? \n");
+    scanf("%f" &populacao);
+    printf("Qual a Área da sua cidade? \n");
+    scanf("%f" &area);
+    printf("Agora informe o PIB da sua cidade: \n");
+    scanf("%f" %pib);
+    printf("Para finalizar, quantos pontos turísticos tem na sua cidade?");
+
+// Agora é hora de informar ao usuário, a carta que ele cadastrou
+// Informando a carta cadastrada.
+
+    printf("Carta: %s %d\n", cidade, estado);
+    printf("População: %f\n");
+    printf("Área: %f\n");
+    printf("PIB: %f\n");
+    printf("Números de pontos turísticos %d\n");
+
+
+
+
+
+
+
+
 
     return 0;
 }
